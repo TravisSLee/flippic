5385c8a9445f8dfc0e29866c8d4075ae30995d52
TravisSLee <travisslee223@gmail.com>
2021-06-19 17:51:32 -0400

    update package.json

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux'; 
+
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Auth0Provider
   domain={ domain }
   clientId={ clientId }
+  store={store}
   redirectUri={window.location.origin}>
-    
+
     <App />
   </Auth0Provider>,
   document.getElementById('root')
