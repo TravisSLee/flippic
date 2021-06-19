8dc31f98ef44f04dc85ddde863cd25f659a7d93d
TravisSLee <travisslee223@gmail.com>
2021-06-19 17:47:29 -0400

    upadate components

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ErrorPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>Sorry page not found!</p>
+            </div>
+        )
+    }
+}
+
+export default ErrorPage
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clietId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 
 ReactDOM.render(
@@ -13,6 +13,7 @@ ReactDOM.render(
   domain={ domain }
   clientId={ clientId }
   redirectUri={window.location.origin}>
+    
     <App />
   </Auth0Provider>,
   document.getElementById('root')
